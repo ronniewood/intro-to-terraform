8d3e553e72d582d802c3ff091621eca5a6a051cd
ronniewood <37124953+ronniewood@users.noreply.github.com>
2020-04-02 21:47:30 -0600

    changed region to us-west-2

--- single-web-server/main.tf
+++ single-web-server/main.tf
@@ -26,7 +26,7 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   # Ubuntu Server 18.04 LTS (HVM), SSD Volume Type in us-east-2
-  ami                    = "ami-0c55b159cbfafe1f0"
+  ami                    = "ami-0ce21b51cb31a48b8"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
