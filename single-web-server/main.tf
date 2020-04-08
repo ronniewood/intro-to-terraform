cf2f8e4a2702d3ecfb75b03b0fa43dbb907fb272
ronniewood <37124953+ronniewood@users.noreply.github.com>
2020-04-07 21:43:24 -0600

    Update main.tf

--- single-web-server/main.tf
+++ single-web-server/main.tf
@@ -20,7 +20,7 @@ terraform {
 provider "aws" {
   region                  = "us-west-2"
   shared_credentials_file = "/user/ronwood/.aws/credentials"
-  profile                 = "terrable2"
+  #profile                 = "terrable2"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
