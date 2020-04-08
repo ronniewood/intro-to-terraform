bf4ab27b8464a80434b4c28c6c90bd64fb93f6a9
ronniewood <37124953+ronniewood@users.noreply.github.com>
2020-04-08 17:23:34 -0600

    deleted the word hello
    deleted hello

--- single-web-server/main.tf
+++ single-web-server/main.tf
@@ -4,7 +4,7 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # ----------------------------------------------------------------------------------------------------------------------
-# hello REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
+# REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
 # This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
 # ----------------------------------------------------------------------------------------------------------------------
 
